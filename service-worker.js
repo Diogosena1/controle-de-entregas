6768c1bba3017f1af9ebb749a599fa5c892dfb56
Diogo Sena <diogosena39@gmail.com>
2025-07-21 03:35:27 -0300

    att2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'controle-entregas-cache-v1'; // ATENÇÃO: Mude 'v1' para 'v2', 'v3', etc., a cada nova versão do seu app!
+const CACHE_NAME = 'controle-entregas-cache-v3'; // ATENÇÃO: Mude 'v1' para 'v2', 'v3', etc., a cada nova versão do seu app!
 const urlsToCache = [
   '/controle-de-entregas/', // Se o index.html for o padrão da pasta
   '/controle-de-entregas/index.html',
